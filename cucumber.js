5e0ead1e930ac24a782dc9208777e2b5f614746f
devcom <kwp@tlen.pl>
2024-12-17 19:36:37 +0100

    first commit

---
+++ cucumber.js
@@ -0,0 +1,8 @@
+module.exports = {
+     default: {
+     require: ["features/**/*.ts"],
+     format: ["progress", "json:report.json"],
+     paths: ["features/**/*.feature"],
+     publishQuiet: true
+     }
+    };
